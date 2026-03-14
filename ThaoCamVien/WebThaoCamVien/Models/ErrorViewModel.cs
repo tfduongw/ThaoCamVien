74cab69de0b548472e70b17a4fd303bf16e3d1e1
tfduongw <nttd482005@gmail.com>
2026-03-14 15:58:53 +0700

    Push toan bo Solution Thao Cam Vien

---
+++ ThaoCamVien/WebThaoCamVien/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebThaoCamVien.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
